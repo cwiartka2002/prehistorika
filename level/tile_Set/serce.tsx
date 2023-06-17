7bd6532a1df3fb62d18323f35b06f0da6f0e49e7
cwiartka2002 <kapibelek@gmail.com>
2023-06-17 22:26:53 +0200

    y

---
+++ level/tile_Set/serce.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="serce" tilewidth="64" tileheight="64" tilecount="42" columns="6">
+ <image source="../../graphics/teren/Antidote/Antidote 1.png" width="423" height="504"/>
+</tileset>
