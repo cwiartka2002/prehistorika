ad8f1286393e602de38563419affd7f12f48b5a0
cwiartka2002 <kapibelek@gmail.com>
2023-06-20 22:59:36 +0200

    ostateczna wersja v.2

---
+++ level/tile_Set/tiles_rock.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles_rock" tilewidth="64" tileheight="64" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile33.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile30.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile34.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile4.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile5.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../graphics/teren/Tiles_rock/tile6.png"/>
+ </tile>
+</tileset>
